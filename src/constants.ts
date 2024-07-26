52679577d337fe18fca0c219a2f593830feb09cd
bowd <dumbogdan@gmail.com>
2024-07-26 09:30:22 +0200

    fix: rename env variable

--- src/constants.ts
+++ src/constants.ts
@@ -4,7 +4,7 @@ export const DUNE_AIRDROP_STATS_QUERY_ID = 3936853;
 
 export const IMPORT_BATCH_SIZE = parseInt(process.env.BATCH_SIZE || "10000");
 export const REDIS_INSERT_CONCURRENCY = parseInt(
-  process.env.REDIS_INSERT_BATCH_SIZE || "1000",
+  process.env.REDIS_INSERT_ONCURRENCY || "10000",
 );
 export const DUNE_API_KEY = process.env.DUNE_API_KEY!;
 export const GOOGLE_PROJECT = process.env.GOOGLE_PROJECT!;
